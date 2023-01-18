73372793ecc677fb79c66eda3a3778cc8b5f1a37
Kiran Evans <thekiranevans@gmail.com>
2023-01-18 12:58:58 +0000

    KE: installed server packages (#1)

---
+++ server/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const cors = require('cors');
+app.use(cors());
+
+const dotenv = require('dotenv');
+dotenv.config('./.env');
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+    credentials: true
+  }
+});
+
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+
+const connectDB = async () => {
+  console.log(`Connecting to MongoDB...`);
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, { // await is necessary
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    return console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    throw new Error(`Failed to connect to MongoDB. ${err}`);
+  }
+}
+
+server.listen(process.env.PORT, async () => {
+    await connectDB();
+    console.log(`Server started on port ${process.env.PORT}`);
+})
