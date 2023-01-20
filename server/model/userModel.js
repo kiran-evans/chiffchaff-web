248bc1614690cb2fd05a87646139d33b81fd8e9d
Kiran Evans <thekiranevans@gmail.com>
2023-01-20 12:13:20 +0000

    Base api (#3)
    * KE: added CRUD to api
    
    * KE: added routes for user

---
+++ server/model/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userModel = mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 6
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 4,
+        maxLength: 15
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+});
+
+module.exports = mongoose.model('User', userModel);
