03a94286c4dd416efa26350ec66cc1599d3a8c50
Kiran Evans <thekiranevans@gmail.com>
2023-01-26 12:35:51 +0000

    KE: users can now request and add contacts using socket (#8)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,31 +9,43 @@ import { AuthContext } from './context/AuthContext';
 import { Navigate } from 'react-router-dom';
 import { useRef } from 'react';
 import { io } from 'socket.io-client';
+import { useEffect } from 'react';
+import { refreshUserData } from './context/UserActions';
 
 function App() {
 
-  const theme = createTheme(themeOptions);
+    const theme = createTheme(themeOptions);
 
-    const { user } = useContext(AuthContext);
+    const { user, dispatch } = useContext(AuthContext);
     const persistedSocket = useRef(io(import.meta.env.ENV_SERVER_URL));
     const socket = persistedSocket.current;
 
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <BrowserRouter>
-        <Box sx={{ height: "100vh", width: "100vw", display: "flex", flexDirection: "column" }}>
-          {user && <Header />}
-          <Box sx={{flex: 1, display: "flex", flexDirection: "column"}}>
-            <Routes>
-              <Route exact path="/" element={user ? <Dashboard socket={socket} /> : <Navigate to="/login" />} />
-              <Route exact path="/login" element={user ? <Navigate to="/" /> : <Splash />} />
-            </Routes>
-          </Box>
-        </Box>
-      </BrowserRouter>
-    </ThemeProvider>
-  )
+    useEffect(() => {
+        socket.on('REFRESH_USER_DATA', () => {
+            refreshUserData(user, dispatch);
+        });
+
+        return () => {
+            socket.off('REFRESH_USER_DATA');
+        }
+    }, []);
+
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <BrowserRouter>
+            <Box sx={{ height: "100vh", width: "100vw", display: "flex", flexDirection: "column" }}>
+                {user && <Header />}
+                <Box sx={{flex: 1, display: "flex", flexDirection: "column"}}>
+                <Routes>
+                    <Route exact path="/" element={user ? <Dashboard socket={socket} /> : <Navigate to="/login" />} />
+                    <Route exact path="/login" element={user ? <Navigate to="/" /> : <Splash />} />
+                </Routes>
+                </Box>
+            </Box>
+            </BrowserRouter>
+        </ThemeProvider>
+    )
 }
 
 export default App
--- server/socketCalls.js
+++ server/socketCalls.js
@@ -11,11 +11,36 @@ module.exports = io => {
             socket.join(user._id.toString());
         });
 
+        socket.on('ALERT', params => {
+            socket.emit('ALERT', { severity: params.severity, text: params.text });
+        });
+
         socket.on('CONTACT_REQUEST', async params => {
             const { userData, contactData } = params;
 
             try {
                 io.sockets.in(contactData._id.toString()).emit('CONTACT_REQUEST', userData);
+                io.sockets.in(contactData._id.toString()).emit('ALERT', { severity: 'info', text: `${userData.username} sent you a contact request.` });
+                
+                io.sockets.in(userData._id.toString()).emit('ALERT', { severity: 'success', text: `Request sent.` });
+            } catch (err) {
+                throw new Error(err);
+            }
+        });
+
+        socket.on('CONTACT_ACCEPT', async params => {
+            const { user1Data, user2Data } = params;
+
+            try {
+                await User.findByIdAndUpdate(user1Data._id, {
+                    contacts: [...user1Data.contacts, user2Data._id.toString()]
+                });
+                io.sockets.in(user1Data._id.toString()).emit('REFRESH_USER_DATA');
+
+                await User.findByIdAndUpdate(user2Data._id, {
+                    contacts: [...user2Data.contacts, user1Data._id.toString()]
+                });
+                io.sockets.in(user2Data._id.toString()).emit('REFRESH_USER_DATA');
 
             } catch (err) {
                 throw new Error(err);
