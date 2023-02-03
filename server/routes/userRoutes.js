b4dcca9e62caf58df44a0398b5137d59b343fd6f
Kiran Evans <thekiranevans@gmail.com>
2023-02-03 11:19:55 +0000

    Server fix 2 (#34)
    * Server fixW
    
    * KE: removed duplicate server src

--- server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createUser, getUser, updateUser, deleteUser, loginUser } = require('../controller/userController');
-
-const router = express.Router();
-
-router.route('/').post(createUser).get(getUser).put(updateUser).delete(deleteUser);
-router.route('/login').post(loginUser);
-
-module.exports = router;
