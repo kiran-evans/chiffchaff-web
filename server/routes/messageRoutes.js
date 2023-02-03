3a86e6ad4d1fd42e73cbdd9f3ca3cc87df01b2b0
kiran-evans <thekiranevans@gmail.com>
2023-02-03 11:19:35 +0000

    KE: removed duplicate server src

--- server/routes/messageRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { createMessage, getMessage, updateMessage, deleteMessage } = require('../controller/messageController');
-
-const router = express.Router();
-
-router.route('/').post(createMessage).get(getMessage).put(updateMessage).delete(deleteMessage);
-
-module.exports = router;
